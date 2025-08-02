ed44af801bad32241d88b6ebc42bee066f28cda6
Iniunam Idorenyin <iniunamid@gmail.com>
2025-08-02 21:25:47 +0000

    chore: set up AppStrings

--- lib/resources/app_strings.dart
+++ lib/resources/app_strings.dart
@@ -1,5 +1,43 @@
 class AppStrings {
   AppStrings._();
 
+  static const appName = 'Quizzia';
+  static const developedByMe = 'Developed by ID';
+  static const onboardingTitle =
+      'Quizzia helps you challenge and assess your knowledge in any field of study';
+  static const onboardingSubTitle =
+      "We’ve got various categories of quizzes, including mathematics, science, anime, books, music and so much more. Let’s get started now";
+  static const getStarted = 'Get Started';
+  static const byVlickingGetStarted =
+      'By clicking Get Started, you agree to our ';
+  static const termsOfService = 'Terms of Service ';
+  static const and = 'and ';
+  static const privacyPolicy = 'Privacy Policy';
+  static const aboutMe = 'About Me';
+  static const enterYourFirstName = 'Enter your first name';
+  static const firstName = 'First Name';
+  static const enterYourOtherNames = 'Enter your  other names';
+  static const otherNames = 'Other Names';
+  static const describeYourself = 'Describe yourself';
+  static const brieflyDescribeYourself =
+      'Briefly describe yourself and your interests';
+  static const submit = 'Submit';
+  static const hi = 'Hi';
+  static const sampleAppUser = 'Emmanuel';
   static const searchForAnything = 'Search for anything';
+  static const categories = 'Categories';
+  static const seeAll = 'See all';
+  static const scoreHistory = 'Score History';
+  static const viewAll = 'View all';
+  static const youHaveNoScoresRecordedYet =
+      'You have no scores recorded yet. Select a category and challenge yourself';
+  static const quizCategories = 'Quiz Categories';
+  static const mathematics = 'Mathematics';
+  static const sports = 'Sports';
+  static const history = 'History';
+  static const animals = 'Animals';
+  static const anime = 'Anime';
+  static const vehicles = 'Vehicles';
+  static const film = 'Film';
+  static const videoGames = 'Video Games';
 }
